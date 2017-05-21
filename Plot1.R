08377e0b178b21f661e304a82a2c6e5d70d0b7f3
Carlos-Ruiz-Ramos <carlos.ruiz.ramos@outlook.com>
2017-05-21 18:04:00 +0200

    Add files via upload

---
+++ Plot1.R
@@ -0,0 +1,27 @@
+
+library(data.table)
+library(png)
+
+setwd("C:/Users/ax25918/Desktop/Trainning/Coursera/Exploratory Data Analysis/Final")
+
+SCC = readRDS("Source_Classification_Code.rds")
+SumarySCC = readRDS("summarySCC_PM25.rds")
+
+SCC = data.table(SCC)
+SumarySCC = data.table(SumarySCC)
+
+Datos = SumarySCC[, sum(Emissions),by=year]
+
+names(Datos)[names(Datos) == "V1"] = "Emissions"
+
+?barplot
+
+png("plot1.png",width=480,height=480)
+barplot(Datos$Emissions, 
+        Datos$year,
+        legend.text = "total PM2.5 emission per year",
+        xlab= "year",
+        names.arg = Datos$year
+        
+)
+dev.off()
